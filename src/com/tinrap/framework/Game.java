0528da2ecf9f6979be72b0d1bb7599485f51073e
Parnit <parnits@gmail.com>
2014-05-03 10:25:25 -0700

    fixed 1 framework file

--- src/com/tinrap/framework/Game.java
+++ src/com/tinrap/framework/Game.java
@@ -4,23 +4,24 @@
 package com.tinrap.framework;
 
 /**
- * @author Parnit
+ * @author Parnit Sainion	
+ * @description: Framework taken from kilobolt.com
+ * @since 3 May 2014
  *
  */
 public interface Game {
 
-	public Audio getAudio();
+    public Audio getAudio();
 
-	public 
- getInput();
+    public Input getInput();
 
-	public FileIO getFileIO();
+    public FileIO getFileIO();
 
-	public Graphics getGraphics();
+    public Graphics getGraphics();
 
-	public void setScreen(Screen screen);
+    public void setScreen(Screen screen);
 
-	public Screen getCurrentScreen();
+    public Screen getCurrentScreen();
 
-	public Screen getInitScreen();
+    public Screen getInitScreen();
 }
